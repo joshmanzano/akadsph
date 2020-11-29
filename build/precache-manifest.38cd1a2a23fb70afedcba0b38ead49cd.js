21153ffdbdff4c9703b5c6295c7e900ac12caf28
joshmanzano <joshuacapulemanzano@gmail.com>
2020-11-29 21:28:17 +0800

    feat: build and CNAME

---
+++ build/precache-manifest.38cd1a2a23fb70afedcba0b38ead49cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff20632706cb6db20704a08c86e39df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7458b76089e2676f2743",
+    "url": "/static/css/2.0878b2bf.chunk.css"
+  },
+  {
+    "revision": "fedcd38bdd9d6775b6eb",
+    "url": "/static/css/main.d6e0e60f.chunk.css"
+  },
+  {
+    "revision": "7458b76089e2676f2743",
+    "url": "/static/js/2.9cc13238.chunk.js"
+  },
+  {
+    "revision": "da5d70772672fcc6471a703893b9d002",
+    "url": "/static/js/2.9cc13238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fedcd38bdd9d6775b6eb",
+    "url": "/static/js/main.5d9cd5ec.chunk.js"
+  },
+  {
+    "revision": "1c0ca4bb65c79d0c874a",
+    "url": "/static/js/runtime-main.fbdf7390.js"
+  },
+  {
+    "revision": "3ff8dc2f8fe7e46945d2f6343d44bbbd",
+    "url": "/static/media/oli.3ff8dc2f.svg"
+  }
+]);
