8dba6294da67a8472c581b1895003857a7f72243
Adrienne Soliven <adriennesoliven@yahoo.com>
2020-12-28 17:26:34 +0800

    fix: request tutorial messages

--- src/components/RequestTutorial.jsx
+++ src/components/RequestTutorial.jsx
@@ -28,72 +28,68 @@ function Tutorial(props) {
 
     const steps = [
       {
-        intro: '<img width="300" src="../static/images/start_oli.gif"/> <center>Welcome to AKADS!</center>'
-      },
-      {
-        intro: '<center><img width="70" src="../static/images/oli-happy.png"/></center> <center>Hello there! My name is Oli, the mascot of AKADS!</center>'
-      },
-      {
-        intro: '<center><img width="70" src="../static/images/oli-wink.png"/></center> <center>I\'m here to guide you in using AKADS, so no need to worry about getting lost!</center>'
+        intro: ' <br/><center><img width="70" src="../static/images/oli-happy.png"/></center> <br/> <center>Hello there! This page is for requesting and matching the best tutors for your child.</center>'
       },
       {
         element: '#topbar',
-        intro: '<center><img width="70" src="../static/images/oli-happy.png"/></center> <center>Up here is the navigation bar, which you can use to explore the website!</center>',
+        intro: ' <br/> <center><img width="70" src="../static/images/oli-happy.png"/></center> <br/> <center>You can always access this page through the navigation bar by clicking on "Request".</center>',
         position: 'right',
         // tooltipClass: 'myTooltipClass',
         // highlightClass: 'myHighlightClass',
       },
       {
-        element: '#topbar',
-        intro: '<center><img width="70" src="../static/images/oli-smirk.png"/></center> <center>There are multiple pages to go through, I\'ll be there to help you in every page!</center>',
+        element: '#tutoringDetails',
+        intro: '<br/> <center><img width="70" src="../static/images/oli-smirk.png"/></center> <br/> <center>Specify the details for the single tutor request here!</center>',
         position: 'right',
         // tooltipClass: 'myTooltipClass',
         // highlightClass: 'myHighlightClass',
       },
       {
-        element: '#overview',
-        intro: '<center><img width="70" src="../static/images/oli-smirk.png"/></center> <center>Right now you are in the "Overview" page, where you can keep track of various information.</center>',
+        element: '#sessionLength',
+        intro: '<br/><center><img width="70" src="../static/images/oli-smirk.png"/></center><br/> <center>Hour credits will be used depending on the length of the session regardless of the subject and tutor option. Ex. A session length of 2 hours will deduct 2 from the hour credits. </center>',
         position: 'right',
         // tooltipClass: 'myTooltipClass',
         // highlightClass: 'myHighlightClass',
       },
       {
-        element: '#calendar',
-        intro: '<center><img width="70" src="../static/images/oli-smirk.png"/></center> <center>Here you can keep track of your upcoming sessions. Days where you have sessions will be <span id="highlighted">highlighted</span>.</center>',
+        element: '#subjectDetails',
+        intro: '<br/> <center><img width="70" src="../static/images/oli-smirk.png"/></center> <br/> <center>Specify the details for the single tutor request here! We highly encourage to send learning materials so our tutors can appropriately prepare.</center>',
         position: 'right',
         // tooltipClass: 'myTooltipClass',
         // highlightClass: 'myHighlightClass',
       },
       {
-        element: '#upcoming',
-        intro: '<center><img width="70" src="../static/images/oli-smirk.png"/></center> <center>Upcoming sessions for the selected date in the calendar will appear here.</center>',
+        element: '#tutoringDetails',
+        intro: '<br/><center><img width="70" src="../static/images/oli-smirk.png"/></center><br/> <center>Once you add a tutor to your favorites, you can specifically request them here next time!</center> <center>You can add tutors in favorite after experiencing at least one session with them.</center>' ,
         position: 'right',
         // tooltipClass: 'myTooltipClass',
         // highlightClass: 'myHighlightClass',
       },
       {
-        element: '#overview',
-        intro: '<center><img width="70" src="../static/images/oli-smirk.png"/></center> <center>Below that, you can see your pending requests, past sessions, and past transactions.</center>',
+        element: '#availability',
+        intro: '<br/><center><img width="70" src="../static/images/oli-smirk.png"/></center><br/> <center>Pick out the possible dates and time your kid is most free for the tutoring session!</center> <center>Note: This is for single sessions and not multiple sessions </center>',
         position: 'right',
         // tooltipClass: 'myTooltipClass',
         // highlightClass: 'myHighlightClass',
       },
       {
-        intro: '<center><img width="70" src="../static/images/oli-smirk.png"/></center> <center>To start off, I suggest to either book a tutor with your free 1 hour, or buy more hours first so you don\'t have to worry about buying in the future.</center>',
+        element: '#specialRequests',
+        intro: '<br/><center><img width="70" src="../static/images/oli-smirk.png"/></center><br/> <center>Feel free to specify any special request here! This is just option if ever.</center>',
         position: 'right',
         // tooltipClass: 'myTooltipClass',
         // highlightClass: 'myHighlightClass',
       },
       {
-        intro: '<center><img width="70" src="../static/images/oli-smirk.png"/></center> <center>I\'ll explain what "requesting a tutor" and "buying hours" means later, go ahead and go to a different page and I\'ll meet you there!</center>',
-        position: 'right',
+        intro: '<br/><center><img width="70" src="../static/images/oli-smirk.png"/></center><br/> <center>Deductions from the credit hours will only be reflected once the request has been accepted. Make sure you have enough hour credits before you make a request!</center>',
         // tooltipClass: 'myTooltipClass',
         // highlightClass: 'myHighlightClass',
       },
       {
-        element: '.floater',
-        intro: 'No matter where you are, you can always click here to ask me for help!',
+        intro: '<br/><center><img width="70" src="../static/images/oli-wink.png"/></center><br/> <center>You will notified as soon possible when a match has been made. Happy Learning!</center>',
+        // tooltipClass: 'myTooltipClass',
+        // highlightClass: 'myHighlightClass',
       },
+      
     ];
 
     return (
