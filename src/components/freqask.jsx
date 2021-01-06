5e38b9dba7e8547a099f4e1bb777d47e11215f07
joshmanzano <josh_manzano@dlsu.edu.ph>
2021-01-06 16:53:36 +0800

    feat: matty and freqask

--- src/components/freqask.jsx
+++ src/components/freqask.jsx
@@ -6,7 +6,7 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
 export class faq extends Component {
 
-  question = (title) => {
+  question = (title, text) => {
     return (
       <Accordion>
         <AccordionSummary
@@ -18,6 +18,7 @@ export class faq extends Component {
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
+            {text}
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -27,26 +28,36 @@ export class faq extends Component {
 
   render() {
     return (
-        <div id="about">
+        <div id="freqask">
         <div className="container">
           <div className="row">
             <div className="col-xs-12 col-md-6"> <img src="img/faq.png" className="img-responsive" alt=""/> </div>
             <div className="col-xs-12 col-md-6">
-            <Grid container direction="column">
+            <Grid container spacing={4} direction="column">
               <Grid item>
-                {this.question('How long does it take to match with a tutor?')}
+                {this.question('How long does it take to match with a tutor?',
+                'It usually takes us around 24 hours or less to find you a tutor. After this matching period, we will allow you to message your tutor for any concerns. Please note, you can only book for a session 2 days after your booking date. This is to give the tutor ample time to prepare the materials for the student.'
+                )}
               </Grid>
               <Grid item>
-                {this.question('What payment methods are accepted?')}
+                {this.question('What payment methods are accepted?',
+                'We accept Gcash, Credit Card, Debit Card, and E-Wallet (e.g. GrabPay, Coins.ph).'
+                )}
               </Grid>
               <Grid item>
-                {this.question('How long can the length of a session be?')}
+                {this.question('How long can the length of a session be?',
+                'A session can last for 1, 2, or 3 hours.'
+                )}
               </Grid>
               <Grid item>
-                {this.question('How do I become a tutor?')}
+                {this.question('How do I become a tutor?',
+                <span>To become a tutor, read through the <a href="#">Become a Tutor</a> page to learn more about our application process.</span>
+                )}
               </Grid>
               <Grid item>
-                {this.question('Do you offer face-to-face tutorials?')}
+                {this.question('Do you offer face-to-face tutorials?',
+                'We do not currently offer face-to-face tutorials.'
+                )}
               </Grid>
             </Grid>
             </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,33 +11,54 @@ export class Header extends Component {
   render() {
     return (
       <header id="header">
+              <Hidden mdDown>
         <div className="intro">
           <div className="overlay">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-8 intro-text">
-                  <h1>
-                    {this.props.data ? this.props.data.title : "Loading"}
-                    <span></span>
-                  </h1>
-                  <p>
-                    Akads helps you match with a tutor that is most suited for your child. <b>Create an account now</b> and have your <b>first hour for FREE.</b>
-                  </p>
-                  <Box mb={8} alignItems="center">
-                      <Button href="#/login" style={{borderRadius: "25px"}} size="large" color="default" variant="outlined">
-                        Get started
-                      </Button>
-                  </Box>
-                </div>
-                <Hidden xsDown>
-                  <div className="intro-text">
-                    <img style={{opacity: 0.2}} width="350" src="../static/images/oli-white.png"></img>
+                <div className="row intro-row">
+                  <div className="col-xs-5 intro-text">
+                    <h1>
+                      {this.props.data ? this.props.data.title : "Loading"}
+                      <span></span>
+                    </h1>
+                    <p>
+                      Akads helps you match with a tutor that is most suited for your child. <b>Create an account now</b> and have your <b>first hour for FREE.</b>
+                    </p>
+                    <Box mb={8} alignItems="center">
+                        <Button href="#/login" style={{borderRadius: "25px"}} size="large" color="default" variant="outlined">
+                          Get started
+                        </Button>
+                    </Box>
                   </div>
-                </Hidden>
-              </div>
+                </div>
             </div>
           </div>
-        </div>
+              </Hidden>
+              <Hidden lgUp>
+        <div className="intro intro-small">
+          <div className="overlay">
+              <Container maxWidth="xs">
+
+                <div className="row">
+                  <div className="intro-text">
+                    <h1>
+                      {this.props.data ? this.props.data.title : "Loading"}
+                      <span></span>
+                    </h1>
+                    <p>
+                      Akads helps you match with a tutor that is most suited for your child. <b>Create an account now</b> and have your <b>first hour for FREE.</b>
+                    </p>
+                    <Box mb={8} alignItems="center">
+                        <Button href="#/login" style={{borderRadius: "25px"}} size="large" color="default" variant="outlined">
+                          Get started
+                        </Button>
+                    </Box>
+                  </div>
+                </div>
+
+              </Container>
+            </div>
+          </div>
+              </Hidden>
       </header>
     );
   }
