ef05c92c4aeaea88586af471dda55b0fbb0a839d
Adrienne Soliven <adriennesoliven@yahoo.com>
2021-01-30 01:16:52 +0800

    style: model for add tutor

---
+++ src/components/OtherTutorDets.jsx
@@ -0,0 +1,153 @@
+import React, {Component} from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import {Paper} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import MuiPhoneNumber from 'material-ui-phone-number';
+import AddIcon from '@material-ui/icons/Add';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import {
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+  FormGroup,
+  FormLabel,
+} from '@material-ui/core';
+
+
+
+const styles = (theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  addChild: {
+    textAlign: 'center',
+  },
+});
+
+class OtherParentDets extends Component{
+
+    constructor(props){
+      super(props);
+      this.state = {
+        rating: 0,
+        status: false,
+        files: '',
+        zoominfo: '',
+        first_time_user: true,
+      }
+      // props.setChild(this.state)
+    }
+
+    changeHandler = (event) => {
+      let nam = event.target.name;
+      let val = event.target.value;
+      this.setState({[nam]: val})
+      // this.setState({[nam]: val}, () => {
+      //   this.props.setChild(this.state)
+      // });
+    }
+
+    handleChange = (event) => {
+      this.setState({[event.target.name]: event.target.checked });
+    };
+
+    render(){
+    const props = this.props;
+    const {classes} = this.props;
+   
+  return (
+    <React.Fragment>
+          <Grid container spacing={2}>
+            
+            <Grid item xs={12} sm={4}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="ratings"
+                label="Ratings"
+                name="ratings"
+                type="number"
+                defaultValue={''}
+                autoComplete="credits"
+                onChange={this.changeHandler}
+              />
+            </Grid>
+            <Grid item xs={12} sm={8}>
+              <TextField
+                autoComplete="files"
+                name="files"
+                variant="outlined"
+                required
+                fullWidth
+                id="files"
+                defaultValue={''}
+                label="Files"
+                autoFocus
+                onChange={this.changeHandler}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12}>
+              <TextField
+                autoComplete="zoominfo"
+                name="zoominfo"
+                variant="outlined"
+                required
+                fullWidth
+                id="zoominfo"
+                defaultValue={''}
+                label="Zoom Info"
+                autoFocus
+                onChange={this.changeHandler}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+            <FormControl component="fieldset" className={classes.formControl}>
+              {/* <FormLabel component="legend">Assign responsibility</FormLabel> */}
+              <FormGroup>
+                
+                <FormControlLabel
+                  control={<Checkbox checked={this.first_time_user} onChange={this.handleChange} name="first_time_user" />}
+                  label="First Time User"
+                />
+                <FormControlLabel
+                  control={<Checkbox checked={this.status} onChange={this.handleChange} name="status" />}
+                  label="Status"
+                />
+              </FormGroup>
+            </FormControl>
+            </Grid>
+            
+          </Grid>
+      </React.Fragment>
+  );
+    }
+}
+
+export default withStyles(styles)(OtherParentDets);
---
+++ src/views/AdminDashboardView/ModalAddTutor.js
@@ -0,0 +1,418 @@
+import React, {useEffect} from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  useTheme,
+  makeStyles,
+  colors,
+  Grid,
+  Tooltip,
+  Button,
+  Box,
+  Container,
+  TextField,
+  InputAdornment,
+  Snackbar,
+  Typography, 
+  IconButton,
+} from '@material-ui/core';
+import {
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    // DialogTitle,
+  } from '@material-ui/core';
+
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+  
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+  
+import CloseIcon from '@material-ui/icons/Close';
+
+import { withStyles } from '@material-ui/core/styles';
+
+
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+
+import OtherParentDets from 'src/components/OtherParentDets';
+import Chip from '@material-ui/core/Chip';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import ModeOfPayment from 'src/views/TutorApp/ModeOfPayment';
+import OtherTutorDets from 'src/components/OtherTutorDets';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100%'
+  },
+  avatar: {
+    backgroundColor: colors.red[600],
+    height: 56,
+    width: 56
+  },
+  differenceIcon: {
+    color: colors.red[900]
+  },
+  differenceValue: {
+    color: colors.red[900],
+    marginRight: theme.spacing(1)
+  },
+  closeButton: {
+    float:'right', marginTop: '5px'
+
+  },
+  dialogTitle:{
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '4px 2px 5px 20px',
+  }, 
+  dialogStyle:{
+    minWidth: "60vh",
+  },
+  appBar: {
+    position: 'relative',
+  },
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+      width: 600,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(1),
+    // marginBottom: theme.spacing(3),
+    padding: theme.spacing(2),
+    
+    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+      marginTop: theme.spacing(6),
+    //   marginBottom: theme.spacing(6),
+      padding: theme.spacing(3),
+    },
+  },
+  stepper: {
+    padding: theme.spacing(3, 0, 5),
+  },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    textAlign: 'right',
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(0),
+  },
+  alreadyLink: {
+    marginBottom: theme.spacing(3),
+    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+      marginBottom: theme.spacing(6),
+    },
+  },
+}));
+
+const ModalAddParent = ({props, open, setOpen, className, ...rest }) => {
+  const classes = useStyles();
+  // const [open, setOpen] = React.useState(false);
+
+  // const [activeStep, setActiveStep] = React.useState(0);
+  // const [accountDetails, setAccount] = React.useState();
+  // const [childDetails, setChild] = React.useState();
+  const subjectselections = ['Math', 'English', 'Filipino', 'Science'];
+  // const [promoDetails, setPromo] = React.useState();
+ 
+  // const steps = ['Account Details', 'Child Details', 'Other Details'];
+
+  // useEffect(() => {
+  //   if(activeStep === steps.length){
+  //     const data = accountDetails;
+  //     data['child'] = childDetails;
+  //     props.register(data)
+  //   }
+  // },[activeStep])
+
+  // function getStepContent(step, props) {
+  //   switch (step) {
+  //     case 0:
+  //       return /*<span>Potato</span>*/ <AccountDetails setAccount={setAccount} {...accountDetails} admin={true}/>;
+  //     case 1:
+  //        return <ChildDetails setChild={setChild} {...childDetails}/>;
+  //     case 2:
+  //       return <OtherParentDets/>;
+  //     default:
+  //       throw new Error('Unknown step');
+  //   }
+  // }
+
+  // const handleNext = () => {
+  //   console.log(activeStep);
+  //   console.log(accountDetails)
+  //   console.log(childDetails)
+  //   // console.log(promoDetails)
+  //   setActiveStep(activeStep + 1);
+  // };
+
+  // const handleBack = () => {
+  //   setActiveStep(activeStep - 1);
+  // };
+
+  const submitHandler = (event, props) => {
+    event.preventDefault();
+    // this.props.register(this.state)
+  }
+
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const DialogTitle = withStyles(useStyles)((props) => {
+    const { children, classes, onClose, ...other } = props;
+    return (
+      <React.Fragment>
+        <MuiDialogTitle disableTypography className={classes.root} {...other}>
+          <Typography variant="h4">{children}</Typography>
+          {onClose ? (
+            <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+              <CloseIcon />
+            </IconButton>
+          ) : null}
+        </MuiDialogTitle>
+        <Divider/>
+        <br/>
+      </React.Fragment>
+    );
+  });
+
+  return (
+    <Dialog
+    open={open}
+    onClose={handleClose}
+    aria-labelledby="alert-dialog-title"
+    aria-describedby="alert-dialog-description"
+    className={classes.dialogStyle}
+    fullWidth={true}
+    maxWidth={'sm'}
+    >
+        <DialogTitle onClose={handleClose} id="alert-dialog-title" className={classes.dialogTitle}>{"Add Tutor"}</DialogTitle>
+        <DialogContent >
+        <Box>
+          <Grid container spacing={3} >
+              <Grid
+                item
+                lg={12}
+                md={12}
+                xl={12}
+                xs={12}
+              >
+                <Typography variant="h3">
+                  Contact Details
+                </Typography>
+              </Grid> 
+              <Grid
+                item
+                lg={6}
+                md={6}
+                xl={6}
+                xs={12}
+              >
+                <TextField id="outlined-basic" label="First Name" variant="outlined" fullWidth/>
+              </Grid> 
+              <Grid
+                item
+                lg={6}
+                md={6}
+                xl={6}
+                xs={12}
+              >
+                <TextField id="outlined-basic" label="Last Name" variant="outlined" fullWidth/>
+              </Grid> 
+              <Grid
+                item
+                lg={6}
+                md={6}
+                xl={6}
+                xs={12}
+              >
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">Sex</FormLabel>
+                  <RadioGroup row name="tutor-sex">
+                    <FormControlLabel value="male" control={<Radio />} label="Male" />
+                    <FormControlLabel value="female" control={<Radio />} label="Female" />
+                    
+                  </RadioGroup>
+                </FormControl>
+              </Grid> 
+              <Grid
+                item
+                lg={6}
+                md={6}
+                xl={6}
+                xs={12}
+              >
+                <TextField id="outlined-basic" label="Birth Date" variant="outlined" type="date" InputLabelProps={{ shrink: true }}fullWidth/>
+              </Grid> 
+
+              <Grid
+                item
+                lg={6}
+                md={6}
+                xl={6}
+                xs={12}
+              >
+                <TextField id="outlined-basic" label="Email Address" type="email" variant="outlined" fullWidth/>
+              </Grid> 
+              <Grid
+                item
+                lg={6}
+                md={6}
+                xl={6}
+                xs={12}
+              >
+                <TextField id="outlined-basic" label="Cellphone Number (+63)" type="phone" variant="outlined"           InputProps={{
+                  startAdornment: <InputAdornment position="start">+63</InputAdornment>,
+                }}fullWidth/>
+              </Grid> 
+              <Grid
+                item
+                lg={12}
+                md={12}
+                xl={12}
+                xs={12}
+              >
+                <Typography variant="h3">
+                  Education Background
+                </Typography>
+              </Grid> 
+              <Grid
+                item
+                lg={6}
+                md={6}
+                xl={6}
+                xs={12}
+              >
+                <TextField id="outlined-basic" label="Tertiary Education" type="college" variant="outlined" placeholder="Ex. Ateneo De Manila University" fullWidth/>
+              </Grid> 
+              {/* <Grid
+                item
+                lg={4}
+                md={4}
+                xl={4}
+                xs={12}
+              >
+                <TextField id="outlined-basic" label="Year of Graduation" type="year" variant="outlined" fullWidth/>
+              </Grid>  */}
+              <Grid
+                item
+                lg={6}
+                md={6}
+                xl={6}
+                xs={12}
+              >
+                <TextField id="outlined-basic" label="Course/Major" type="course" variant="outlined" placeholder="Ex. BS Information Technology Entrepreneurship" fullWidth/>
+              </Grid> 
+              <Grid
+                item
+                lg={12}
+                md={12}
+                xl={12}
+                xs={12}
+              >
+                <TextField id="outlined-basic" label="Notable Achievements" type="course" variant="outlined" placeholder="Ex. Consistent Dean's Lister" fullWidth/>
+              </Grid>
+              <Grid
+                item
+                lg={12}
+                md={12}
+                xl={12}
+                xs={12}
+              >
+                <Autocomplete
+                  multiple
+                  id="tags-filled"
+                  options={subjectselections.map((option) => option)}
+                  freeSolo
+                  variant="outlined"
+                  renderTags={(value, getTagProps) =>
+                    value.map((option, index) => (
+                      <Chip variant="outlined" label={option} {...getTagProps({ index })} />
+                    ))
+                  }
+                  renderInput={(params) => (
+                    <TextField {...params} variant="outlined" label="Subjects Teaching" />
+                  )}
+                />
+              </Grid> 
+              <Grid
+                item
+                lg={12}
+                md={12}
+                xl={12}
+                xs={12}
+              >
+                <ModeOfPayment/>
+              </Grid>
+              <Grid
+                item
+                lg={12}
+                md={12}
+                xl={12}
+                xs={12}
+              >
+                <Typography variant="h3">
+                  Other Details
+                </Typography>
+              </Grid> 
+              <Grid
+                item
+                lg={12}
+                md={12}
+                xl={12}
+                xs={12}
+              >
+                <OtherTutorDets/>
+              </Grid> 
+            </Grid>
+
+
+          </Box>
+        </DialogContent>
+        
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+              Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus>
+              Save
+          </Button>
+        </DialogActions>
+    </Dialog>
+  );
+};
+
+ModalAddParent.propTypes = {
+  className: PropTypes.string
+};
+
+export default ModalAddParent;
