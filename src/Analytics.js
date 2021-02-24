ca305e20c4e428ddfc5250e6ffb800d8f4bc6d94
joshmanzano <josh_manzano@dlsu.edu.ph>
2021-02-24 18:25:34 +0800

    fix: attempt to fix GTM and FBP

---
+++ src/Analytics.js
@@ -0,0 +1,18 @@
+console.log('Running Facebook Pixel code...')
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '332566451426441'); 
+fbq('track', 'PageView');
+
+console.log('Running GTM code...')
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);};
+gtag('js', new Date());
+gtag('config', 'G-VPQXR69SYS');
+
